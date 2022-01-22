d19d459b5eeb510357931514aa994711cd37e94b
Pankaj Kumar <pankaj.kumar49111@gmail.com>
2022-01-22 18:33:15 +0530

    update

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -4,7 +4,7 @@ function Note() {
   return (
     <div className='note'>
       <h1>This is a Title</h1>
-      <p>This is the content</p>
+      <p>This is the content from me !!!</p>
     </div>
   )
 }
