68df0b255d414afb1f3c41698fa44f367cba01be
Pankaj Kumar <pankaj.kumar49111@gmail.com>
2022-01-29 18:00:12 +0530

    some styling

--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -1,6 +1,10 @@
 import React, { useState } from 'react'
+import AddIcon from '@material-ui/icons/Add'
+import Fab from '@material-ui/core/Fab'
+import Zoom from '@material-ui/core/Zoom'
 
 function CreateArea(props) {
+  const [isExpanded, setIsExpanded] = useState(false)
   const [note, setNote] = useState({
     title: '',
     content: '',
@@ -10,13 +14,16 @@ function CreateArea(props) {
     const { name, value } = event.target
 
     setNote((prevNote) => {
-      return { ...prevNote, [name]: value }
+      return {
+        ...prevNote,
+        [name]: value,
+      }
     })
   }
 
   function submitNote(event) {
     event.preventDefault()
-    if (note.title && note.content) {
+    if (note.content) {
       props.onAdd(note)
       setNote({
         title: '',
@@ -25,23 +32,33 @@ function CreateArea(props) {
     }
   }
 
+  function expand() {
+    setIsExpanded(true)
+  }
   return (
     <div>
-      <form>
-        <input
-          name='title'
-          placeholder='Title'
-          value={note.title}
-          onChange={handleChange}
-        />
+      <form className='create-note'>
+        {isExpanded ? (
+          <input
+            name='title'
+            onChange={handleChange}
+            value={note.title}
+            placeholder='Title'
+          />
+        ) : null}
         <textarea
           name='content'
-          placeholder='Take a note...'
-          rows='3'
-          value={note.content}
           onChange={handleChange}
+          onClick={expand}
+          value={note.content}
+          placeholder='Take a note...'
+          rows={isExpanded ? 3 : 1}
         />
-        <button onClick={submitNote}>Add</button>
+        <Zoom in={isExpanded}>
+          <Fab onClick={submitNote}>
+            <AddIcon />
+          </Fab>
+        </Zoom>
       </form>
     </div>
   )
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import AddAlertIcon from '@material-ui/icons/AddAlert'
 
 function Header() {
   return (
     <header>
-      <h1>Keeper</h1>
+      <h1>
+        <AddAlertIcon /> Keeper
+      </h1>
     </header>
   )
 }
